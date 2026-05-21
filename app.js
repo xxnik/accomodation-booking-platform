a519a2b2d83a764a594e5c0e600a74f20be4456d
Delta Student <223nikhilanand@gmail.com>
2026-05-21 22:30:50 +0530

    fixing render issue

--- app.js
+++ app.js
@@ -82,6 +82,7 @@
             expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true,
+            sameSite: "lax",
         }
     }
 
